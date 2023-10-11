159057cadb43b911a778f9439f99d8e6b9952865
juns1s <juns1s@naver.com>
2023-10-12 02:23:04 +0900

    feat: GoogleDataDto 구현

---
+++ src/main/java/com/milkcow/tripai/member/dto/GoogleDataDto.java
@@ -0,0 +1,11 @@
+package com.milkcow.tripai.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GoogleDataDto {
+    private String nickname;
+    private String email;
+}
