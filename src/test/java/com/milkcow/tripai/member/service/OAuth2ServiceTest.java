b3d43a404d32f90ab7377764981c20657c5f25b6
juns1s <juns1s@naver.com>
2023-10-13 14:37:31 +0900

    chore: 토큰 위치 수정

--- src/test/java/com/milkcow/tripai/member/service/OAuth2ServiceTest.java
+++ src/test/java/com/milkcow/tripai/member/service/OAuth2ServiceTest.java
@@ -15,12 +15,13 @@ class OAuth2ServiceTest {
     private OAuth2Service oAuth2Service;
     @Autowired
     private MemberRepository memberRepository;
+    private String token = "accessToken";
     @Test
     public void 구글_회원가입() throws Exception{
         //given
 
         //when
-        oAuth2Service.oAuth2Login("ya29.a0AfB_byD257ZD7mPZNZYUM2jED77_RW6DMmbAze27EYrIh9DGfEfini8eSMCI3etkD7l-I-WgpMtJJ-K90UtvSUvM8mnQhcK6e52poxs5IybRl4_Teyn8YJQUoiL8DfwlxhtwXwUtQwqmDCd1oBTEbXlxKrmYf40iti-paCgYKAcsSARASFQGOcNnCwnG4lisjeYw1kulqaVqWIw0171", null);
+        oAuth2Service.oAuth2Login(token, null);
         //then
         Member member = memberRepository.findByEmail("junjuns1s1s@gmail.com").get();
         System.out.println("member.getNickname() = " + member.getNickname());
@@ -29,9 +30,8 @@ class OAuth2ServiceTest {
     @Test
     public void 구글_로그인() throws Exception{
         //given
-        oAuth2Service.oAuth2Login("ya29.a0AfB_byD257ZD7mPZNZYUM2jED77_RW6DMmbAze27EYrIh9DGfEfini8eSMCI3etkD7l-I-WgpMtJJ-K90UtvSUvM8mnQhcK6e52poxs5IybRl4_Teyn8YJQUoiL8DfwlxhtwXwUtQwqmDCd1oBTEbXlxKrmYf40iti-paCgYKAcsSARASFQGOcNnCwnG4lisjeYw1kulqaVqWIw0171", null);
-        oAuth2Service.oAuth2Login("ya29.a0AfB_byD257ZD7mPZNZYUM2jED77_RW6DMmbAze27EYrIh9DGfEfini8eSMCI3etkD7l-I-WgpMtJJ-K90UtvSUvM8mnQhcK6e52poxs5IybRl4_Teyn8YJQUoiL8DfwlxhtwXwUtQwqmDCd1oBTEbXlxKrmYf40iti-paCgYKAcsSARASFQGOcNnCwnG4lisjeYw1kulqaVqWIw0171", null);
-
+        oAuth2Service.oAuth2Login(token, null);
+        oAuth2Service.oAuth2Login(token, null);
         //when
         //then
     }
