02381f72468b39c84082137d2d5b77512a3d953d
juns1s <juns1s@naver.com>
2023-10-16 18:25:44 +0900

    feat: 항공권 조회 서비스 테스트

---
+++ src/test/java/com/milkcow/tripai/plan/service/FlightPlanServiceTest.java
@@ -0,0 +1,24 @@
+package com.milkcow.tripai.plan.service;
+
+import com.milkcow.tripai.plan.dto.FlightDataDto;
+import org.assertj.core.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class FlightPlanServiceTest {
+    @Autowired
+    private FlightPlanService flightPlanService;
+    
+    @Test
+    public void 항공권_조회() throws Exception{
+        //given
+        //when
+        FlightDataDto flightData = flightPlanService.getFlightData("ICN", "LAX", "20231225");
+        //then
+        System.out.println("flightData.getFlightDataList() = " + flightData.getFlightDataList());
+        Assertions.assertThat(flightData.getFlightCount()).isPositive();
+    }
+    
+}
