d63bdfbff227679b2525fa877e22841972fe27f3
juns1s <juns1s@naver.com>
2023-10-31 22:05:43 +0900

    refactor: FlightPlanServiceNaverImpl 구현체 분리

--- src/test/java/com/milkcow/tripai/plan/service/FlightPlanServiceNaverImplTest.java
+++ src/test/java/com/milkcow/tripai/plan/service/FlightPlanServiceNaverImplTest.java
@@ -7,15 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 
 @SpringBootTest
-class FlightPlanServiceTest {
+class FlightPlanServiceNaverImplTest {
     @Autowired
-    private FlightPlanService flightPlanService;
+    private FlightPlanServiceNaverImpl flightPlanServiceNaverImpl;
     
     @Test
     public void 항공권_조회() throws Exception{
         //given
         //when
-        FlightDataDto flightData = flightPlanService.getFlightData("LAX", "ICN", "20231225", 2000000);
+        FlightDataDto flightData = flightPlanServiceNaverImpl.getFlightData("LAX", "ICN", "20231225", 2000000);
         //then
         System.out.println("flightData.getFlightDataList() = " + flightData.getFlightDataList());
         Assertions.assertThat(flightData.getFlightCount()).isPositive();
