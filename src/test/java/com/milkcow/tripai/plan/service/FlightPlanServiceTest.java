2734d156c7d141d60bc8accd7758c87b44ef98fd
juns1s <juns1s@naver.com>
2023-11-04 02:22:38 +0900

    chore: 클래스 명 수정

--- src/test/java/com/milkcow/tripai/plan/service/FlightPlanServiceTest.java
+++ src/test/java/com/milkcow/tripai/plan/service/FlightPlanServiceTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlightPlanServiceAPIImplTest {
+class FlightPlanServiceTest {
 
     @Autowired
     private FlightPlanService flightPlanServiceAPI;
