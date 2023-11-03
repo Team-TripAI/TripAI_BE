fdad639ad1e7b77f5392360fd4cb632de41a963f
juns1s <juns1s@naver.com>
2023-11-04 02:24:03 +0900

    chore: Data, Builder 어노테이션 추가

--- src/main/java/com/milkcow/tripai/plan/dto/AccommodationDataDto.java
+++ src/main/java/com/milkcow/tripai/plan/dto/AccommodationDataDto.java
@@ -1,7 +1,11 @@
 package com.milkcow.tripai.plan.dto;
 
 import java.util.List;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class AccommodationDataDto {
     private Integer AccommodationCount;
     private List<AccommodationData> accommodationDataList;
