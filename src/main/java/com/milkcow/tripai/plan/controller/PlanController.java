2e1a19ee11a06bb5abb0f87c97efa53ec34d73cd
juns1s <juns1s@naver.com>
2023-11-04 03:17:47 +0900

    rename: 폴더 정리 및 클래스명 단순화

--- src/main/java/com/milkcow/tripai/plan/controller/PlanController.java
+++ src/main/java/com/milkcow/tripai/plan/controller/PlanController.java
@@ -5,8 +5,8 @@ import com.milkcow.tripai.plan.dto.AccommodationDataDto;
 import com.milkcow.tripai.plan.dto.FlightDataDto;
 import com.milkcow.tripai.plan.exception.PlanException;
 import com.milkcow.tripai.plan.result.PlanResult;
-import com.milkcow.tripai.plan.service.AccommodationPlanService;
-import com.milkcow.tripai.plan.service.FlightPlanService;
+import com.milkcow.tripai.plan.service.accommodation.AccommodationService;
+import com.milkcow.tripai.plan.service.flight.FlightService;
 import com.milkcow.tripai.plan.util.DateValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/plan/budget")
 public class PlanController {
 
-    private final FlightPlanService flightPlanService;
-    private final AccommodationPlanService accommodationPlanService;
+    private final FlightService flightService;
+    private final AccommodationService accommodationService;
 
     @GetMapping("/flight")
     public DataResponse<FlightDataDto> getFlightPlan(@RequestParam String departureAirport,
@@ -30,7 +30,7 @@ public class PlanController {
         if (!DateValidator.isValidDate(departure)) {
             throw new PlanException(PlanResult.INVALID_DATE);
         }
-        FlightDataDto flightData = flightPlanService.getFlightData(departureAirport, arrivalAirport, departure,
+        FlightDataDto flightData = flightService.getFlightData(departureAirport, arrivalAirport, departure,
                 maxFare);
         return DataResponse.create(flightData, PlanResult.OK_FLIGHT_PLAN);
     }
@@ -43,7 +43,7 @@ public class PlanController {
         if (!DateValidator.isValidDate(startDate) || !DateValidator.isValidDate(endDate)) {
             throw new PlanException(PlanResult.INVALID_DATE);
         }
-        AccommodationDataDto accommodationData = accommodationPlanService.getAccommodationData(destination, startDate,
+        AccommodationDataDto accommodationData = accommodationService.getAccommodationData(destination, startDate,
                 endDate, maxPrice);
         return DataResponse.create(accommodationData, PlanResult.OK_ACCOMMODATION_PLAN);
     }
