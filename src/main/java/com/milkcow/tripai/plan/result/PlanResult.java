c7c11da5ff95926dcf1b47f019f0a47b8a2e8c76
juns1s <juns1s@naver.com>
2023-11-03 20:13:51 +0900

    feat: API limit 만료 경우 추가

--- src/main/java/com/milkcow/tripai/plan/result/PlanResult.java
+++ src/main/java/com/milkcow/tripai/plan/result/PlanResult.java
@@ -10,9 +10,11 @@ import org.springframework.http.HttpStatus;
 public enum PlanResult implements ResultProvider {
     OK_FLIGHT_PLAN(200, HttpStatus.OK, "항공권 조회 성공"),
 
+    API_KEY_LIMIT_EXCESS(429, HttpStatus.UNAUTHORIZED, "항공권 API 만료"),
+
     FLIGHT_API_REQUEST_FAILED(510, HttpStatus.INTERNAL_SERVER_ERROR, "항공권 API 요청 실패"),
     FLIGHT_API_RESPONSE_EMPTY(520, HttpStatus.INTERNAL_SERVER_ERROR, "빈 항공권 API응답"),
-    FLIGHT_API_RESPONSE_INVALID(530, HttpStatus.INTERNAL_SERVER_ERROR, "유효하지 않은 항공권 API응답")
+    FLIGHT_API_RESPONSE_INVALID(530, HttpStatus.INTERNAL_SERVER_ERROR, "유효하지 않은 항공권 API응답"),
     ;
 
     private final Integer code;
