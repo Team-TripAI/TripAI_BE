90d9a552e2f95b886f13564eaa7991a434198209
juns1s <juns1s@naver.com>
2023-11-03 23:03:18 +0900

    chore: API키 만료시 http 메소드 변경

--- src/main/java/com/milkcow/tripai/plan/result/PlanResult.java
+++ src/main/java/com/milkcow/tripai/plan/result/PlanResult.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum PlanResult implements ResultProvider {
     OK_FLIGHT_PLAN(200, HttpStatus.OK, "항공권 조회 성공"),
 
-    API_KEY_LIMIT_EXCESS(429, HttpStatus.UNAUTHORIZED, "항공권 API 만료"),
+    API_KEY_LIMIT_EXCESS(540, HttpStatus.UNAUTHORIZED, "항공권 API 만료"),
 
     FLIGHT_API_REQUEST_FAILED(510, HttpStatus.INTERNAL_SERVER_ERROR, "항공권 API 요청 실패"),
     FLIGHT_API_RESPONSE_EMPTY(520, HttpStatus.INTERNAL_SERVER_ERROR, "빈 항공권 API응답"),
