38c90c5dfef8e019864bf713a32157b77916c326
juns1s <juns1s@naver.com>
2023-11-05 01:40:55 +0900

    feat: 날짜를 다루기 위한 DateUtil 구현

--- src/main/java/com/milkcow/tripai/plan/controller/PlanController.java
+++ src/main/java/com/milkcow/tripai/plan/controller/PlanController.java
@@ -7,7 +7,7 @@ import com.milkcow.tripai.plan.exception.PlanException;
 import com.milkcow.tripai.plan.result.PlanResult;
 import com.milkcow.tripai.plan.service.accommodation.AccommodationService;
 import com.milkcow.tripai.plan.service.flight.FlightService;
-import com.milkcow.tripai.plan.util.DateValidator;
+import com.milkcow.tripai.plan.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +27,7 @@ public class PlanController {
                                                      @RequestParam String arrivalAirport,
                                                      @RequestParam String departure,
                                                      @RequestParam(defaultValue = "0x7fffffff") int maxFare) {
-        if (!DateValidator.isValidDate(departure)) {
+        if (!DateUtil.isValidDate(departure)) {
             throw new PlanException(PlanResult.INVALID_DATE);
         }
         FlightDataDto flightData = flightService.getFlightData(departureAirport, arrivalAirport, departure,
@@ -40,7 +40,7 @@ public class PlanController {
                                                                    @RequestParam String startDate,
                                                                    @RequestParam String endDate,
                                                                    @RequestParam(defaultValue = "0x7fffffff") int maxPrice) {
-        if (!DateValidator.isValidDate(startDate) || !DateValidator.isValidDate(endDate)) {
+        if (!DateUtil.isValidDate(startDate) || !DateUtil.isValidDate(endDate)) {
             throw new PlanException(PlanResult.INVALID_DATE);
         }
         AccommodationDataDto accommodationData = accommodationService.getAccommodationData(destination, startDate,
