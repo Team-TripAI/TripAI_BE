7de66973a3ca894e97c1621a88f9dfe4ffc3ee88
juns1s <juns1s@naver.com>
2023-11-06 03:41:33 +0900

    chore: 가격대 수정

--- src/main/java/com/milkcow/tripai/plan/embedded/PriceRange.java
+++ src/main/java/com/milkcow/tripai/plan/embedded/PriceRange.java
@@ -1,11 +1,12 @@
 package com.milkcow.tripai.plan.embedded;
 
 public enum PriceRange {
-    FIVE("$", 1, 1, 0, 5),
-    FIVE_TO_TEN("$~$$", 1, 2, 5, 10),
-    TEN_TO_TWENTY("$~$$$", 1, 3, 10, 20),
-    MORE_THAN_TWENTY("$~$$$$", 1, 4, 20, Integer.MAX_VALUE),
+    FIVE("$", 1, 1, 0, 50000),
+    FIVE_TO_TEN("$$", 1, 2, 50000, 100000),
+    TEN_TO_TWENTY("$$$", 1, 3, 100000, 200000),
+    MORE_THAN_TWENTY("$$$$", 1, 4, 200000, Integer.MAX_VALUE),
     ;
+
     private final String priceRange;
     private final int minDollar;
     private final int maxDollar;
@@ -30,6 +31,10 @@ public enum PriceRange {
         } else {
             return MORE_THAN_TWENTY;
         }
+    }
+
+    public int getMaxDollar() {
+        return maxDollar;
     }
 
     public boolean isInRange(int dollarCount) {
