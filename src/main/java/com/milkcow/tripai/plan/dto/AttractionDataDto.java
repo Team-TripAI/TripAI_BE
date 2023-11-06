9eed4e29797cc1a4be8fcc8385c6ddf5022db208
juns1s <juns1s@naver.com>
2023-11-06 15:27:20 +0900

    style: 명소 스웨거 설정

--- src/main/java/com/milkcow/tripai/plan/dto/AttractionDataDto.java
+++ src/main/java/com/milkcow/tripai/plan/dto/AttractionDataDto.java
@@ -1,6 +1,7 @@
 package com.milkcow.tripai.plan.dto;
 
 import com.milkcow.tripai.plan.embedded.AttractionData;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.Data;
 @Data
 @Builder
 public class AttractionDataDto {
+    @ApiModelProperty(value = "조회된 명소 개수", example = "2")
     private int AttractionCount;
     private List<AttractionData> attractionDataList;
 }
--- src/main/java/com/milkcow/tripai/plan/embedded/AttractionData.java
+++ src/main/java/com/milkcow/tripai/plan/embedded/AttractionData.java
@@ -1,20 +1,25 @@
 package com.milkcow.tripai.plan.embedded;
 
-import java.util.ArrayList;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 @Builder
 public class AttractionData {
+    @ApiModelProperty(value = "명소 명", example = "Meiji Jingu Shrine")
     private String name;
+    @ApiModelProperty(value = "위도", example = "35.676167")
     private Double lat;
+    @ApiModelProperty(value = "경도", example = "139.69952")
     private Double lng;
+    @ApiModelProperty(value = "예약 가능 활동 개수", example = "2")
     private Integer offerCount;
     private List<AttractionOffer> offerList;
+    @ApiModelProperty(value = "예약 가능 활동 가격대", example = "$$$")
     private String price;
     private List<PlaceHour> hours;
+    @ApiModelProperty(value = "이미지 url", example = "https://media-cdn.tripadvisor.com/media/photo-l/11/a2/c7/9d/caption.jpg")
     private String image;
 }
