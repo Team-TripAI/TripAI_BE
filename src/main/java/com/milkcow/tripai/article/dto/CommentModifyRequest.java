66fe51e762f1272bb5181523717114868a71c31b
clap_0 <clap-0@naver.com>
2023-11-06 16:50:20 +0900

    style: 댓글 관련 스웨거 설정

--- src/main/java/com/milkcow/tripai/article/dto/CommentModifyRequest.java
+++ src/main/java/com/milkcow/tripai/article/dto/CommentModifyRequest.java
@@ -1,5 +1,8 @@
 package com.milkcow.tripai.article.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +15,13 @@ import javax.validation.constraints.Size;
 @Builder
 @RequiredArgsConstructor
 @NoArgsConstructor(force = true)
+@Schema
 public class CommentModifyRequest {
 
     @NotNull
     @Size(max = 200)
+    @ApiParam(value = "댓글 내용\n" +
+            "최대 200자")
+    @ApiModelProperty(example = "이야 해초 보소")
     private final String content;
 }
