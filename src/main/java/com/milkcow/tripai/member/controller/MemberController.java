57b8cef1e3c4c995e5d2ae21da7105742a09bbd9
Park - Jeonghwan <990706leo@gmail.com>
2023-11-14 22:01:31 +0900

    fix : Member 관련 스웨거 설정

--- src/main/java/com/milkcow/tripai/member/controller/MemberController.java
+++ src/main/java/com/milkcow/tripai/member/controller/MemberController.java
@@ -5,7 +5,6 @@ import com.milkcow.tripai.global.dto.DataResponse;
 import com.milkcow.tripai.global.dto.ResponseDto;
 import com.milkcow.tripai.jwt.JwtService;
 import com.milkcow.tripai.member.domain.Member;
-import com.milkcow.tripai.member.dto.MemberLoginRequestDto;
 import com.milkcow.tripai.member.dto.MemberSignupRequestDto;
 import com.milkcow.tripai.member.dto.MemberUpdateRequestDto;
 import com.milkcow.tripai.member.dto.MemberWithdrawRequestDto;
@@ -22,7 +21,6 @@ import io.swagger.annotations.ApiResponses;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -37,7 +35,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "Member")
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
@@ -45,7 +42,6 @@ public class MemberController {
     private final CustomUserDetailsService userDetailsService;
 
     private final JwtService jwtService;
-
 
     /**
      * Email을 통한 회원가입
@@ -57,7 +53,7 @@ public class MemberController {
     @Transactional
     @ApiOperation(value = "Email을 통한 회원가입", notes = "이메일을 아이디로 사용한 회원가입이다.")
     @ApiImplicitParam(name = "MemberSignupRequestDto", value = "이메일, 비밀번호, 닉네임",
-            required = true, paramType = "Body")
+            paramType = "body", dataType = "com/milkcow/tripai/member/dto/MemberSignupRequestDto.java")
     @ApiResponses({
             @ApiResponse(code = 200, message = "회원가입 성공!"),
             @ApiResponse(code = 419, message = "이미 존재하는 회원입니다."),
@@ -70,10 +66,10 @@ public class MemberController {
         return DataResponse.of(true, MemberResult.OK_SIGNUP);
     }
 
-    @PostMapping("/login")
-    public void fakeLogin(@RequestBody MemberLoginRequestDto requestDto) {
-        throw new IllegalStateException("Spring Security 필터로 구동되는 API 입니다.");
-    }
+//    @PostMapping("/login")
+//    public void fakeLogin(@RequestBody MemberLoginRequestDto requestDto) {
+//        throw new IllegalStateException("Spring Security 필터로 구동되는 API 입니다.");
+//    }
 
     /**
      * 회원 정보 수정
@@ -85,7 +81,7 @@ public class MemberController {
     @PostMapping("/users")
     @Transactional
     @ApiOperation(value = "회원 정보 수정", notes = "현재는 Nickname만 수정 가능하다.")
-    @ApiImplicitParam(name = "MemberUpdateRequestDto", value = "닉네임, 비밀번호", required = true, paramType = "Body")
+    @ApiImplicitParam(name = "MemberUpdateRequestDto", value = "닉네임, 비밀번호", paramType = "body", dataType = "com/milkcow/tripai/member/dto/MemberUpdateRequestDto.java")
     @ApiResponses({
             @ApiResponse(code = 200, message = "닉네임 수정 성공!"),
             @ApiResponse(code = 500, message = "서버 내 오류")
@@ -111,7 +107,7 @@ public class MemberController {
     @DeleteMapping("/users")
     @Transactional
     @ApiOperation(value = "회원 탈퇴", notes = "회원 탈퇴를 진행한다.")
-    @ApiImplicitParam(name = "MemberWithdrawRequestDto", value = "비밀번호", required = true, paramType = "Body")
+    @ApiImplicitParam(name = "withdrawDto", value = "비밀번호", paramType = "body", dataType = "com/milkcow/tripai/member/dto/MemberWithdrawRequestDto.java")
     @ApiResponses({
             @ApiResponse(code = 200, message = "회원탈퇴 성공!"),
             @ApiResponse(code = 500, message = "서버 내 오류")
