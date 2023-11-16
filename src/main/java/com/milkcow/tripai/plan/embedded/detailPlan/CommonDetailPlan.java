4ca0e3bd0adaa012fbc27ad0d1e3d6b0b209418e
juns1s <juns1s@naver.com>
2023-11-17 00:47:46 +0900

    feat: 숙박, 맛집, 명소 공통 정보 생성자 구현

--- src/main/java/com/milkcow/tripai/plan/embedded/detailPlan/CommonDetailPlan.java
+++ src/main/java/com/milkcow/tripai/plan/embedded/detailPlan/CommonDetailPlan.java
@@ -2,10 +2,17 @@ package com.milkcow.tripai.plan.embedded.detailPlan;
 
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+/**
+ * 숙박, 맛집, 명소 공통 정보 저장 추상 클래스
+ */
 @Embeddable
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class CommonDetailPlan {
     @NotNull
     private String name;
