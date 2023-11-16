8cce258f699448f5a00de87c1855ed8d718717bc
juns1s <juns1s@naver.com>
2023-11-17 00:46:07 +0900

    feat: 항공권 정보 생성자 구현

--- src/main/java/com/milkcow/tripai/plan/embedded/detailPlan/FlightPlan.java
+++ src/main/java/com/milkcow/tripai/plan/embedded/detailPlan/FlightPlan.java
@@ -1,6 +1,7 @@
 package com.milkcow.tripai.plan.embedded.detailPlan;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -8,12 +9,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 항공권 정보 저장 값타입
+ */
 @Embeddable
 @Getter
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
-public class FlightPlan extends CommonDetailPlan{
+@AllArgsConstructor
+public class FlightPlan{
+    @NotNull
+    private String name;
     @NotNull
     private String airline;
     @NotNull
@@ -25,5 +31,6 @@ public class FlightPlan extends CommonDetailPlan{
     @NotNull
     private LocalDateTime endTime;
     @NotNull
+    @Column(length = 350)
     private String url;
 }
