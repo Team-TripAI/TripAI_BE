f54f70feac9ab8c13f850ec9eb4b5b7c21c3d914
juns1s <juns1s@naver.com>
2023-11-17 02:48:54 +0900

    refactor: 일정 등록 성공 이름 변경

--- src/main/java/com/milkcow/tripai/plan/result/PlanResult.java
+++ src/main/java/com/milkcow/tripai/plan/result/PlanResult.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum PlanResult implements ResultProvider {
-    OK_PLAN_REGISTER(201, HttpStatus.CREATED, "일정 등록 성공"),
+    OK_PLAN_CREATE(201, HttpStatus.CREATED, "일정 등록 성공"),
     OK_PLAN_FIND(200, HttpStatus.OK, "개별 일정 조회 성공"),
 
     PLAN_FORBIDDEN(403, HttpStatus.FORBIDDEN, "권한 없음"),
