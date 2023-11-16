e798a84f79c17ab627c2f25254d2550409c6f11c
juns1s <juns1s@naver.com>
2023-11-17 00:45:12 +0900

    feat: 숙박 정보 생성자 구현

--- src/main/java/com/milkcow/tripai/plan/embedded/detailPlan/AccommodationPlan.java
+++ src/main/java/com/milkcow/tripai/plan/embedded/detailPlan/AccommodationPlan.java
@@ -2,18 +2,28 @@ package com.milkcow.tripai.plan.embedded.detailPlan;
 
 import java.time.LocalDate;
 import javax.persistence.Embeddable;
-import lombok.AllArgsConstructor;
+import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 숙박 정보 저장 값타입
+ */
 @Embeddable
 @Getter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class AccommodationPlan extends CommonDetailPlan{
     private String image;
     private LocalDate startDate;
     private LocalDate endDate;
+
+    @Builder
+    public AccommodationPlan(@NotNull String name, double lat, double lng, String image,
+                             LocalDate startDate, LocalDate endDate) {
+        super(name, lat, lng);
+        this.image = image;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }
