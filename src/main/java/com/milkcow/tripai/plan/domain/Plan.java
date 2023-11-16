067bfe5e10cc3d6a88ada895083e5814b8e1d25a
juns1s <juns1s@naver.com>
2023-11-17 00:50:31 +0900

    feat: dto -> 엔티티 구현

--- src/main/java/com/milkcow/tripai/plan/domain/Plan.java
+++ src/main/java/com/milkcow/tripai/plan/domain/Plan.java
@@ -1,10 +1,12 @@
 package com.milkcow.tripai.plan.domain;
 
 import com.milkcow.tripai.member.domain.Member;
+import com.milkcow.tripai.plan.dto.PlanDto;
 import com.milkcow.tripai.plan.embedded.detailPlan.AccommodationPlan;
 import com.milkcow.tripai.plan.embedded.detailPlan.AttractionPlan;
 import com.milkcow.tripai.plan.embedded.detailPlan.FlightPlan;
 import com.milkcow.tripai.plan.embedded.detailPlan.RestaurantPlan;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +23,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 예산기반 일정 엔티티
+ */
 @Entity
 @Builder
 @NoArgsConstructor
@@ -35,10 +40,9 @@ public class Plan {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalDateTime createDateTime;
     private String name;
     private int totalBudget;
     private int accommodationBudget;
@@ -62,4 +66,28 @@ public class Plan {
             joinColumns = @JoinColumn(name = "plan_id"))
     private List<AttractionPlan> attractionPlanList = new ArrayList<>();
 
+    /**
+     * 여행 계획 DTO를 엔티티로 변환
+     * @param request {@link PlanDto} 여행 계획 DTO
+     * @param member {@link Member} 여행 계획 등록 사용자
+     * @return {@link Plan} Plan 엔티티
+     */
+    public static Plan of(PlanDto request, Member member){
+        return Plan.builder()
+                .member(member)
+                .startDate(request.getStart())
+                .endDate(request.getEnd())
+                .createDateTime(LocalDateTime.now())
+                .name(request.getName())
+                .totalBudget(request.getTotal())
+                .accommodationBudget(request.getAccommodation())
+                .flightBudget(request.getFlight())
+                .restaurantBudget(request.getRestaurant())
+                .attractionBudget(request.getAttraction())
+                .accommodationPlanList(request.getAccommodationList())
+                .flightPlanList(request.getFlightList())
+                .restaurantPlanList(request.getRestaurantList())
+                .attractionPlanList(request.getAttractionList())
+                .build();
+    }
 }
