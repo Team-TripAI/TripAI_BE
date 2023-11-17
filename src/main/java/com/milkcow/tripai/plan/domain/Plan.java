8a57e85d51fb9691d77bd4b81329abf25944c21a
juns1s <juns1s@naver.com>
2023-11-17 17:07:28 +0900

    feat: dto -> entity구현

--- src/main/java/com/milkcow/tripai/plan/domain/Plan.java
+++ src/main/java/com/milkcow/tripai/plan/domain/Plan.java
@@ -1,23 +1,25 @@
 package com.milkcow.tripai.plan.domain;
 
 import com.milkcow.tripai.member.domain.Member;
-import com.milkcow.tripai.plan.dto.PlanDto;
-import com.milkcow.tripai.plan.embedded.detailPlan.AccommodationPlan;
-import com.milkcow.tripai.plan.embedded.detailPlan.AttractionPlan;
-import com.milkcow.tripai.plan.embedded.detailPlan.FlightPlan;
-import com.milkcow.tripai.plan.embedded.detailPlan.RestaurantPlan;
+import com.milkcow.tripai.plan.dto.PlanRequestDto;
+import com.milkcow.tripai.plan.dto.accommodation.AccommodationPlanDto;
+import com.milkcow.tripai.plan.dto.attraction.AttractionPlanDto;
+import com.milkcow.tripai.plan.dto.flight.FlightPlanDto;
+import com.milkcow.tripai.plan.dto.restaurant.RestaurantPlanDto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CollectionTable;
+import java.util.stream.Collectors;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -49,31 +51,62 @@ public class Plan {
     private int flightBudget;
     private int restaurantBudget;
     private int attractionBudget;
-    @ElementCollection
-    @CollectionTable(name = "accommodationPlan",
-            joinColumns = @JoinColumn(name = "plan_id"))
+
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<AccommodationPlan> accommodationPlanList = new ArrayList<>();
-    @ElementCollection
-    @CollectionTable(name = "flightPlan",
-            joinColumns = @JoinColumn(name = "plan_id"))
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<FlightPlan> flightPlanList = new ArrayList<>();
-    @ElementCollection
-    @CollectionTable(name = "restaurantPlan",
-            joinColumns = @JoinColumn(name = "plan_id"))
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<RestaurantPlan> restaurantPlanList = new ArrayList<>();
-    @ElementCollection
-    @CollectionTable(name = "attractionPlan",
-            joinColumns = @JoinColumn(name = "plan_id"))
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<AttractionPlan> attractionPlanList = new ArrayList<>();
+
+    private void setDetailPlanList(List<AccommodationPlan> accommodationPlanList,
+                                   List<FlightPlan> flightPlanList,
+                                   List<RestaurantPlan> restaurantPlanList,
+                                   List<AttractionPlan> attractionPlanList) {
+        this.accommodationPlanList = accommodationPlanList;
+        this.flightPlanList = flightPlanList;
+        this.restaurantPlanList = restaurantPlanList;
+        this.attractionPlanList = attractionPlanList;
+    }
 
     /**
      * 여행 계획 DTO를 엔티티로 변환
-     * @param request {@link PlanDto} 여행 계획 DTO
-     * @param member {@link Member} 여행 계획 등록 사용자
+     *
+     * @param request {@link PlanRequestDto} 여행 계획 DTO
+     * @param member  {@link Member} 여행 계획 등록 사용자
      * @return {@link Plan} Plan 엔티티
      */
-    public static Plan of(PlanDto request, Member member){
-        return Plan.builder()
+    public static Plan of(PlanRequestDto request, Member member) {
+        Plan plan = defaultPlanBuild(request, member);
+
+        parseDtoToEntity(request, plan);
+        return plan;
+    }
+
+    private static void parseDtoToEntity(PlanRequestDto request, Plan plan) {
+        List<AccommodationPlanDto> accommodationList = request.getAccommodationList();
+        List<FlightPlanDto> flightList = request.getFlightList();
+        List<RestaurantPlanDto> restaurantList = request.getRestaurantList();
+        List<AttractionPlanDto> attractionList = request.getAttractionList();
+
+        List<AccommodationPlan> accommodationPlans = accommodationList.stream().map(a -> AccommodationPlanDto.toEntity(a,
+                        plan))
+                .collect(Collectors.toList());
+
+        List<FlightPlan> flightPlans = flightList.stream().map(a -> FlightPlanDto.toEntity(a, plan))
+                .collect(Collectors.toList());
+        List<RestaurantPlan> restaurantPlans = restaurantList.stream().map(a -> RestaurantPlanDto.toEntity(a, plan))
+                .collect(Collectors.toList());
+        List<AttractionPlan> attractionPlans = attractionList.stream().map(a -> AttractionPlanDto.toEntity(a, plan))
+                .collect(Collectors.toList());
+
+        plan.setDetailPlanList(accommodationPlans, flightPlans, restaurantPlans, attractionPlans);
+    }
+
+    private static Plan defaultPlanBuild(PlanRequestDto request, Member member) {
+        Plan plan = Plan.builder()
                 .member(member)
                 .startDate(request.getStart())
                 .endDate(request.getEnd())
@@ -84,10 +117,7 @@ public class Plan {
                 .flightBudget(request.getFlight())
                 .restaurantBudget(request.getRestaurant())
                 .attractionBudget(request.getAttraction())
-                .accommodationPlanList(request.getAccommodationList())
-                .flightPlanList(request.getFlightList())
-                .restaurantPlanList(request.getRestaurantList())
-                .attractionPlanList(request.getAttractionList())
                 .build();
+        return plan;
     }
 }
