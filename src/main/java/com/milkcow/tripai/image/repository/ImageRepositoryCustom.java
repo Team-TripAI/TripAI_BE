b9239a49c1e61a63f40fa9dc439a90de4456aa85
juns1s <juns1s@naver.com>
2023-11-21 15:16:41 +0900

    chore: 리포지토리 어노테이션 추가

--- src/main/java/com/milkcow/tripai/image/repository/ImageRepositoryCustom.java
+++ src/main/java/com/milkcow/tripai/image/repository/ImageRepositoryCustom.java
@@ -3,7 +3,9 @@ package com.milkcow.tripai.image.repository;
 import com.milkcow.tripai.image.dto.ImageResponseData;
 import com.milkcow.tripai.image.embedded.Color;
 import java.util.List;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepositoryCustom {
     List<ImageResponseData> searchSimilar(List<String> labelList, List<Color> colorList);
 }
