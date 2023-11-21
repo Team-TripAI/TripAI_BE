6058bd596ace6960f39fb6880f5619e5a4a30d14
juns1s <juns1s@naver.com>
2023-11-22 00:27:30 +0900

    feat: ImageService 구현

--- src/main/java/com/milkcow/tripai/image/service/ImageService.java
+++ src/main/java/com/milkcow/tripai/image/service/ImageService.java
@@ -7,6 +7,7 @@ import com.milkcow.tripai.image.dto.ImageResponseData;
 import com.milkcow.tripai.image.dto.ImageResponseDto;
 import com.milkcow.tripai.image.embedded.Color;
 import com.milkcow.tripai.image.repository.ImageRepositoryCustom;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -18,15 +19,13 @@ public class ImageService {
     private final ImageRepositoryCustom imageRepository;
 
     public DataResponse<ImageResponseDto> searchSimilarPlace(ImageRequestDto requestDto) {
-        //TODO 임시 코드입니다. 리포지토리에서 받은 임의의 값을 그대로 사용합니다.
-        // 추천 알고리즘 작성해주세요~
         List<String> requestLabelList = requestDto.getLabelList();
         List<Color> requestColorList = requestDto.stringToColor();
 
         List<Image> labelMatchedList = imageRepository.searchLabelMatch(requestLabelList);
 
-        List<ImageResponseData> recommendList = labelMatchedList.stream().map(ImageResponseData::toDto)
-                .collect(Collectors.toList());
+        List<ImageResponseData> recommendList = calculateSimilarImage(requestLabelList, requestColorList,
+                labelMatchedList);
 
         int recommendCount = recommendList.size();
         ImageResponseDto responseDto = ImageResponseDto
@@ -35,5 +34,16 @@ public class ImageService {
                 .recommendList(recommendList)
                 .build();
         return DataResponse.create(responseDto);
+    }
+
+    private List<ImageResponseData> calculateSimilarImage(List<String> requestLabelList,
+                                                          List<Color> requestColorList,
+                                                          List<Image> labelMatchedList) {
+        return labelMatchedList
+                .stream()
+                .map(i -> new ImageScore(requestLabelList, requestColorList, i))
+                .sorted(Comparator.comparing(ImageScore::getScore).reversed())
+                .map(ImageScore::toDto)
+                .collect(Collectors.toList());
     }
 }
