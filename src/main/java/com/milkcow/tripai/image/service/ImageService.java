1bf3042cff6fe0e76834897b414bfea73021bc4f
juns1s <juns1s@naver.com>
2023-11-21 15:23:16 +0900

    feat: 리포지토리 반환 값 Image로 변경

--- src/main/java/com/milkcow/tripai/image/service/ImageService.java
+++ src/main/java/com/milkcow/tripai/image/service/ImageService.java
@@ -1,11 +1,13 @@
 package com.milkcow.tripai.image.service;
 
 import com.milkcow.tripai.global.dto.DataResponse;
+import com.milkcow.tripai.image.domain.Image;
 import com.milkcow.tripai.image.dto.ImageRequestDto;
 import com.milkcow.tripai.image.dto.ImageResponseData;
 import com.milkcow.tripai.image.dto.ImageResponseDto;
 import com.milkcow.tripai.image.repository.ImageRepositoryCustom;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +19,11 @@ public class ImageService {
     public DataResponse<ImageResponseDto> searchSimilarPlace(ImageRequestDto requestDto) {
         //TODO 임시 코드입니다. 리포지토리에서 받은 임의의 값을 그대로 사용합니다.
         // 추천 알고리즘 작성해주세요~
-        List<ImageResponseData> recommendList = imageRepository.searchSimilar(requestDto.getLabelList(),
+        List<Image> labelMatchedList = imageRepository.searchSimilar(requestDto.getLabelList(),
                 requestDto.stringToColor());
+        
+        List<ImageResponseData> recommendList = labelMatchedList.stream().map(ImageResponseData::toDto)
+                .collect(Collectors.toList());
 
         int recommendCount = recommendList.size();
         ImageResponseDto responseDto = ImageResponseDto
