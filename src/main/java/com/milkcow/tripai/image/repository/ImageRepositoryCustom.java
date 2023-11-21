508be100de3949c741ffbc1fa5f2ceecaab14200
juns1s <juns1s@naver.com>
2023-11-21 15:23:01 +0900

    feat: 리포지토리 반환 값 Image로 변경

--- src/main/java/com/milkcow/tripai/image/repository/ImageRepositoryCustom.java
+++ src/main/java/com/milkcow/tripai/image/repository/ImageRepositoryCustom.java
@@ -1,11 +1,11 @@
 package com.milkcow.tripai.image.repository;
 
-import com.milkcow.tripai.image.dto.ImageResponseData;
+import com.milkcow.tripai.image.domain.Image;
 import com.milkcow.tripai.image.embedded.Color;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImageRepositoryCustom {
-    List<ImageResponseData> searchSimilar(List<String> labelList, List<Color> colorList);
+    List<Image> searchSimilar(List<String> labelList, List<Color> colorList);
 }
